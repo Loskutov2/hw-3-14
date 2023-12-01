e46272d093e98f4a3bfbb1e59c0ee685934a2aba
elka <112241103+Loskutov2@users.noreply.github.com>
2023-12-01 17:17:49 +0200

    dsdsadsaasd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+const myName='Yelisey'
+const src = 'https://i.dailymail.co.uk/1s/2021/02/23/10/39658340-9290299-Alyson_Kalhagen_from_Green_Bay_was_contacted_after_posting_pictu-m-50_1614077506130.jpg'
+const alt = 'cat'
+const obj = {
+  name:'Telegram',
+  url:'https://web.telegram.org'
+}
+const a=2
+const b=2
+const colors = [
+  "green",
+  "yellow",
+  "purple",
+  "red",
+  "violet",
+  "orange",
+  "black"
+]
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{fontSize:40}}>
+      <h1>{myName}</h1>
+      <p>Лакаво просимо до нашого сайту</p>
+      <img src={src} alt={alt} />
+      <a href={obj.url}>{obj.name}</a>
+      <p>{a}+{b}={a+b}</p>
+      <ul>
+        {colors.map((color)=>{
+          return <li>{color}</li>
+        })}
+      </ul>
     </div>
   );
 };
